be96b0436d747b24a7038c5245db055723469880
vdogamer <ctd8z@virginia.edu>
2015-01-14 16:46:24 +0000

    Adding devise to thinkful reporting

---
+++ app/models/admin.rb
@@ -0,0 +1,18 @@
+class Admin < ActiveRecord::Base
+  
+  validates :username, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]*\z/, message: "may only contain letters and numbers." }
+  attr_accessor :login
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
+end
