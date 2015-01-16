8223629bca10f5665492c4e282d0aca2a2d61dd9
vdogamer <ctd8z@virginia.edu>
2015-01-16 20:37:49 +0000

    Getting ready to deploy to heroku

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'spring',        group: :development
 
 gem 'devise'
 
+group :production do
+  gem 'rails_12factor'
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
   # get 'users/index'
-  match '/users', to: 'users#index', via: 'get'
-  match '/users/id', to: 'users#show', via: 'get'
-  match '/users/sign_out', to: 'devise/sessions#destroy', via: 'delete'
+  
 
   devise_for :admins, :controllers => { :registrations => "registrations"}
   devise_for :users, :controllers => { :registrations => "registrations"}  
-  resources :users, :only =>[:show]
+  resources :users  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'users#index'
+  root :to => 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
