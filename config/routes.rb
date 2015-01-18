488ab61d7933d929032ee39964e3234c66198208
vdogamer <ctd8z@virginia.edu>
2015-01-18 02:12:49 +0000

    Bootstrap is added but not to the splash page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root :to => 'home#index'
+  # root :to => 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
